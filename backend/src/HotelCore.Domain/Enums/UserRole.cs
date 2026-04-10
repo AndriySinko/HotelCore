42e92837809feb5bacdcc549907f5603ce35d0a6
Synko Andrii <sinkoandriy7@gmail.com>
2026-04-10 09:44:00 +0200

    feat(domain): UserRole enum

---
+++ backend/src/HotelCore.Domain/Enums/UserRole.cs
@@ -0,0 +1,17 @@
+// This file contains code for UserRole.
+namespace HotelCore.Domain.Enums;
+
+
+
+
+
+public enum UserRole
+{
+    Guest = 0,
+    Receptionist = 1,
+    CleaningWorker = 2,
+    KitchenStaff = 3,
+    HotelManager = 4,
+    Supervisor = 5,
+    Administrator = 6
+}
