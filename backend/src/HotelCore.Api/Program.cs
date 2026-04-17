b30a846c77de81855dcf85fad97722a3ddc9af76
Synko Andrii <sinkoandriy7@gmail.com>
2026-04-17 10:22:00 +0200

    chore(api): app startup and seeding

---
+++ backend/src/HotelCore.Api/Program.cs
@@ -0,0 +1,84 @@
+// This file contains code for Program.
+using HotelCore.Infrastructure;
+using HotelCore.Api.Middlewares;
+using HotelCore.Application;
+using HotelCore.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using HotelCore.Infrastructure.Persistence;
+using HotelCore.ServiceDefaults;
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.ConfigureLogging();
+
+builder.AddServiceDefaults();
+
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+builder.Services.ConfigureVersioning();
+builder.Services.ConfigureOpenApi();
+builder.Services.ConfigureMvc();
+builder.Services.AddApplication();
+builder.AddInfrastructure();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+builder.Services.ConfigureAuthentication(builder.Configuration);
+
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+
+app.UseOpenApi();
+
+app.UseExceptionHandler();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
+
+await app.ResetTestDataAsync();  
+await app.SeedRolesAsync();
+await app.SeedAdminUserAsync();
+await app.SeedTestUsersAsync();
+await app.SeedCleaningWorkersAsync();
+await app.SeedRoomsAsync();
+
+app.Run();
